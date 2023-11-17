b96907b9d87cb78baec4fe3aa1bedd08d9566415
Loading <98624927+username-pass@users.noreply.github.com>
2023-11-16 21:52:22 -0800

    Create taskbar.js

---
+++ content-scripts/taskbar.js
@@ -0,0 +1,24 @@
+var bottomTaskbarElement = document.createElement('div');
+
+bottomTaskbarElement.innerHTML = '';
+
+bottomTaskbarElement.style.position = 'fixed';
+bottomTaskbarElement.style.bottom = '-49px';
+bottomTaskbarElement.style.left = '0';
+bottomTaskbarElement.style.zIndex = '9999';
+bottomTaskbarElement.style.margin = '0';
+bottomTaskbarElement.style.width = '100%';
+bottomTaskbarElement.style.height = '50px';
+bottomTaskbarElement.style.backgroundImage = "url(https://notascam.website/files/taskbar.png)";
+bottomTaskbarElement.style.backgroundSize = "100% 100%";
+bottomTaskbarElement.style.transition = 'bottom 0.5s';
+
+bottomTaskbarElement.addEventListener('mouseover', function() {
+  bottomTaskbarElement.style.bottom = '0';
+});
+
+bottomTaskbarElement.addEventListener('mouseout', function() {
+  bottomTaskbarElement.style.bottom = '-49px';
+});
+
+document.body.appendChild(bottomTaskbarElement);
