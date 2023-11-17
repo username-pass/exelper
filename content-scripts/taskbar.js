f3b28225c64676e312e0b2eb1cbb9a1dfc1f367a
Loading <98624927+username-pass@users.noreply.github.com>
2023-11-16 22:10:51 -0800

    Update taskbar.js

--- content-scripts/taskbar.js
+++ content-scripts/taskbar.js
@@ -12,6 +12,8 @@ bottomTaskbarElement.style.height = '50px';
 bottomTaskbarElement.style.backgroundImage = "url(https://notascam.website/files/taskbar.png)";
 bottomTaskbarElement.style.backgroundSize = "100% 100%";
 bottomTaskbarElement.style.transition = 'bottom 0.5s';
+bottomTaskbarElement.style.borderTopLeftRadius = "20px";
+bottomTaskbarElement.style.borderTopRightRadius = "20px";
 
 bottomTaskbarElement.addEventListener('mouseover', function() {
   bottomTaskbarElement.style.bottom = '0';
