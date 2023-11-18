132faf32774e55ec9e88701a96c7ff5189e3c98f
Loading <98624927+username-pass@users.noreply.github.com>
2023-11-18 01:05:39 +0000

    Update taskbar.js

--- content-scripts/taskbar.js
+++ content-scripts/taskbar.js
@@ -221,7 +221,7 @@ bottomTaskbarElement.innerHTML = `<div id="taskbar">
           const hours = now.getHours().toString().padStart(2, '0');
           const minutes = now.getMinutes().toString().padStart(2, '0');
           const seconds = now.getSeconds().toString().padStart(2, '0');
-          const time = \`${hours}:${minutes}\`; //\`:${seconds}\`;
+          const time = \`\${hours}:\${minutes}\`; //\`:\${seconds}\`;
 
           actualTime.innerText = time;
         }
